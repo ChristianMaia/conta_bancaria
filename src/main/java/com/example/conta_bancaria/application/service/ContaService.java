9f9311c3b38cdd9af9e183e174c06b836703d806
Christian Maia <christiansoaresmaia@gmail.com>
2025-09-25 11:03:16 -0300

    Conta começado, boa time 25/09/25

--- src/main/java/com/example/conta_bancaria/application/service/ContaService.java
+++ src/main/java/com/example/conta_bancaria/application/service/ContaService.java
@@ -1,8 +1,11 @@
 package com.example.conta_bancaria.application.service;
 
+import com.example.conta_bancaria.application.dto.ContaResumoDTO;
 import com.example.conta_bancaria.domain.entity.Cliente;
+import com.example.conta_bancaria.domain.repository.ContaRepository;
 import com.example.conta_bancaria.domain.repository.CorrenteRepository;
 import com.example.conta_bancaria.domain.repository.PoupancaRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,9 +17,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
+@RequiredArgsConstructor
+@Transactional
 @Service
 public class ContaService {
+    private final ContaRepository repository;
 
+    @Transactional(readOnly = true)
+    public List<ContaResumoDTO> listarTodasContas() {
+        return repository.findAllByAtivaTrue().stream()
+                .filter(Conta::isAtiva)
+                .map(ContaResumoDTO::FromEntity).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public ContaResumoDTO buscarContaPorNumero(String numero) {
+        return ContaResumoDTO.FromEntity(
+                repository.findByNumeroAndAtivaTrue(numero)
+                        .orElseThrow(() -> new RuntimeException("Conta não encontrada"))
+        );
+    }
 }
 
